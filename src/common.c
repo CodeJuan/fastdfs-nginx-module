23da890846392e3fcf9cc14e77a436e95755f3a8
happyfish100 <happyfish100@7151d7cb-efd4-16d2-fc0f-02b8f60cfcc9>
2011-03-06 07:52:07 +0000

    support url parameter "filename" to specify attachment filename

--- src/common.c
+++ src/common.c
@@ -180,7 +180,7 @@ int fdfs_mod_init()
 	load_local_host_ip_addrs();
 	fdfs_get_params_from_tracker();
 	
-	logInfo("fastdfs nginx module v1.04, " \
+	logInfo("fastdfs apache / nginx module v1.04, " \
 		"response_mode=%s, " \
 		"base_path=%s, " \
 		"connect_timeout=%d, "\
